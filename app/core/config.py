d7796bcdd6bef72a0452e562583bc35c0d88be25
shama-llama <eyobgeletu@tutanota.com>
2025-07-09 15:56:59 +0300

    Restructure paths

--- app/core/config.py
+++ app/core/config.py
@@ -7,16 +7,17 @@ APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 # Main directories
 LOG_DIR = os.path.join(APP_ROOT, "logs")
 OUTPUT_DIR = os.path.join(APP_ROOT, "output")
-MODEL_DIR = os.path.join(APP_ROOT, "model")
+MAPPINGS_DIR = os.path.join(APP_ROOT, "mappings")
+ONTOLOGIES_DIR = os.path.join(APP_ROOT, "ontologies")
 
-# Model/config files
-LANGUAGE_MAPPING_PATH = os.path.join(MODEL_DIR, "language_mapping.json")
-CODE_QUERIES_PATH = os.path.join(MODEL_DIR, "code_queries.json")
-CARRIER_TYPES_PATH = os.path.join(MODEL_DIR, "carrier_types.json")
-EXCLUDED_DIRECTORIES_PATH = os.path.join(MODEL_DIR, "excluded_directories.json")
-CONTENT_TYPES_PATH = os.path.join(MODEL_DIR, "content_types.json")
-WEB_DEV_ONTOLOGY_PATH = os.path.join(MODEL_DIR, "web_development_ontology.owl")
-BASIC_FORMAL_ONTOLOGY_PATH = os.path.join(MODEL_DIR, "basic_formal_ontology.owl")
+# Mapping/config files
+LANGUAGE_MAPPING_PATH = os.path.join(MAPPINGS_DIR, "language_mapping.json")
+CODE_QUERIES_PATH = os.path.join(MAPPINGS_DIR, "code_queries.json")
+CARRIER_TYPES_PATH = os.path.join(MAPPINGS_DIR, "carrier_types.json")
+EXCLUDED_DIRECTORIES_PATH = os.path.join(MAPPINGS_DIR, "excluded_directories.json")
+CONTENT_TYPES_PATH = os.path.join(MAPPINGS_DIR, "content_types.json")
+WEB_DEV_ONTOLOGY_PATH = os.path.join(ONTOLOGIES_DIR, "web_development_ontology.owl")
+BASIC_FORMAL_ONTOLOGY_PATH = os.path.join(ONTOLOGIES_DIR, "basic_formal_ontology.owl")
 
 # Ontology cache filename (for use in paths)
 ONTOLOGY_CACHE_FILENAME = "ontology_cache.json"
